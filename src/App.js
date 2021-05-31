bef3d025720e495f5592be8b69a99a066fc95398
Sergey <sergeyboy1377@gmail.com>
2021-05-31 23:14:32 +0300

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
  import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Footer from "./components/Footer";
-import Content from './components/Content';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Footer from "./components/Footer/Footer";
+import Content from './components/Content/Content';
 
 
 function App() {
---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import s from './Content.module.css';
+import MyPost from './My Post/Post/MyPost';
+
+
+function Content() {
+  return (
+    <div className={s.content}>
+      <div className={s.fond}>
+        <img src="https://images.samsung.com/is/image/samsung/p5/ru/explore/landscape-photography/Explore_How_To_Take_Great_Landscape_Photography_KV.jpg?$ORIGIN_JPG$" alt="fond"/>
+      </div>
+      <div className={s.profile}>
+        <div className={s.avatar}>
+          <img src="https://i0.wp.com/hairstylesweekly.com/images/2012/06/Ryan-Seacrest-Fauxhawk.jpg?fit=395%2C582&ssl=1" alt="avatar"/>
+        </div>
+        <div className={s.description}>
+          <div className={s.name}>John Dou</div>
+          <div className={s.birthday}>День рождения: 22.10.1999</div>
+          <div className={s.city}>Город: Odessa</div>
+          <div className={s.education}>Образование: OGU Mechnikova`2020</div>
+          <div className={s.site}>
+            <a href="https://aquaplus.od.ua">Web Site: https://aquaplus.od.ua</a>
+          </div>
+        </div>
+      </div>
+      <MyPost/>
+    </div>
+);
+}
+
+export default Content;
