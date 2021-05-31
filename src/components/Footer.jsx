eb03de0e3fb3ba849ad319cbadf89df5286bb4b3
Sergey <sergeyboy1377@gmail.com>
2021-05-31 21:11:14 +0300

    module_css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Footer.module.css'
 
 function Footer() {
   return (
-    <footer className="footer">
-      <a className="company" href="#"> Company © 2021 Company.od.ua Все права защищены.</a>
+    <footer className={s.footer}>
+      <a className={s.company} href="#"> Company © 2021 Company.od.ua Все права защищены.</a>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import s from './Header.module.css'
 
 function Header() {
   return (
-    <header className="header">
+    <header className={s.header}>
     <a href="#">
-      <img className="logo" src="https://www.pngfind.com/pngs/m/84-841512_hacking-logo-png-logo-hacker-png-transparent-png.png" alt="logo"/>
+      <img className={s.logo} src="https://www.pngfind.com/pngs/m/84-841512_hacking-logo-png-logo-hacker-png-transparent-png.png" alt="logo"/>
     </a>
-  <div className="login">
-    <div className="menu entered"><a href="#">Вход</a></div>
-    <div className="menu registration"><a href="#">Регистрация</a></div>
+  <div className={s.login}>
+    <div className={s.menu}><a href="#">Вход</a></div>
+    <div className={s.menu}><a href="#">Регистрация</a></div>
   </div>
     </header>)
 }
