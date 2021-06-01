751144b1ec5677ba3c8aad4182d89a09a60897d1
Sergey <sergeyboy1377@gmail.com>
2021-06-01 20:57:33 +0300

    PROPS

--- src/components/Content/My Post/Post/Post.jsx
+++ src/components/Content/My Post/Post/Post.jsx
@@ -2,29 +2,24 @@ import React from 'react';
 import s from './Post.module.css';
 
 
-
-function Post() {
+function Post(props) {
   return (
     <div>
       <div className={s.list}>
         <div><img className={s.paint}
                   src="https://socialniesety.ru/files/images/components/articles_journal/originals/instagram/403/kak_sdelat_krasivuyu_avatarku_v_instagram_1.jpg"
                   alt=""/></div>
-        <div className={s.message}>
-          <div className="s.item">Hi buddy, how are you?</div>
+        <div className="s.item">{props.message}
+          <div>
+            <span>{props.LikeCount}</span>
+          </div>
         </div>
+
       </div>
-      <div className={s.list}>
-        <div><img className={s.paint}
-                  src="https://wobs.ua/content/images/1/makiyazh-baby-face-46370986877517_small4.jpg"
-                  alt=""/></div>
-        <div className={s.message}>
-          <div className="s.item">Привет, как жизнь?</div>
-        </div>
-      </div>
+
     </div>
 
-);
+  );
 }
 
 export default Post;
