8deabe5028f1e6691b6dc95b04320bc954f336b0
Sergey <sergeyboy1377@gmail.com>
2021-06-03 22:38:04 +0300

    Dialogs NavLink

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom';
 
 function Dialogs(props) {
-  return(
-    <div>
-      Dialog
-    </div>
-  )
-}
-export default Dialogs;
+    return (
+      <div className={s.dialogs}>
+        <div className={s.dialogsItem}>
+          <div className={s.dialog + ' ' + s.active}><NavLink to='/dialogs/7'>Dmitriy</NavLink></div>
+          <div className={s.dialog}><NavLink to='/dialogs/1'>Andrey</NavLink></div>
+          <div className={s.dialog}><NavLink to='/dialogs/2'>Sasha</NavLink></div>
+          <div className={s.dialog}><NavLink to='/dialogs/3'>Viktor</NavLink></div>
+          <div className={s.dialog}><NavLink to='/dialogs/4'>Valera</NavLink></div>
+          <div className={s.dialog}><NavLink to='/dialogs/5'>Igor</NavLink></div>
+          <div className={s.dialog}><NavLink to='/dialogs/6'> Irina</NavLink></div>
+          <div className={s.dialog}><NavLink to='/dialogs/7'>Tatyana</NavLink></div>
+        </div>
+        <div className={s.messages}>
+          <div className={s.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis dolore esse, hic id optio sapiente sint.
+          </div>
+          <div className={s.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis dolore esse,
+          </div>
+          <div className={s.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis dolore esse,
+          </div>
+          <div className={s.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis dolore esse,
+          </div>
+          <div className={s.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis dolore esse,
+          </div>
+          <div className={s.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis dolore esse,
+          </div>
+          <div className={s.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis dolore esse,
+          </div>
+          <div className={s.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis dolore esse,
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+  export default Dialogs;
