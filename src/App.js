025372e6fa625f9d1c465fad7d446fd96839e944
boysergey <65461241+boysergey@users.noreply.github.com>
2021-06-11 11:00:04 +0300

    redux (file 'state')

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Content from './components/Content/Content';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Friends from './components/Friends/Friends';
 import Groups from './components/Groups/Groups';
 import News from './components/News/News';
 import Music from './components/Music/Music'
 
 function App(props) {
-  return (
-    <BrowserRouter> {
-      <div className="app-wrapper">
-        <Header/>
-        <wrapper>
-          <Navbar/>
+    return (
+        <BrowserRouter> {
+            <div className="app-wrapper">
+                <Header/>
+                <div className='section'>
+                    <Navbar/>
 
-          <div className="app-wrapper_content">
-            <Route path="/dialogs" render={() => <Dialogs dialogsData = {props.dialogsData} messagesData = {props.messagesData}/>}/>
-            <Route path="/content" render={() => <Content postsData = {props.postsData}/>}/>
-            <Route path="/friends" render={() => <Friends/>}/>
-            <Route path="/groups" render={() => <Groups/>}/>
-            <Route path="/news" render={() => <News/>}/>
-            <Route path="/music" render={() => <Music/>}/>
-          </div>
-        </wrapper>
-        <Footer/>
-      </div>
-    }</BrowserRouter>
+                    <div className="app-wrapper_content">
+                        <Route path="/dialogs" render={() => <Dialogs dialogsData={props.state.dialogsData}
+                                                                      messagesData={props.state.messagesData}/>}/>
+                        <Route path="/content" render={() => <Content postsData={props.state.postsData}/>}/>
+                        <Route path="/friends" render={() => <Friends/>}/>
+                        <Route path="/groups" render={() => <Groups/>}/>
+                        <Route path="/news" render={() => <News/>}/>
+                        <Route path="/music" render={() => <Music/>}/>
+                    </div>
+                </div>
+                <Footer/>
+            </div>
+        }</BrowserRouter>
 
-  );
+    );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,16 @@ import s from './Header.module.css'
 function Header() {
   return (
     <header className={s.header}>
-    <a href="#">
-      <img className={s.logo} src="https://www.pngfind.com/pngs/m/84-841512_hacking-logo-png-logo-hacker-png-transparent-png.png" alt="logo"/>
-    </a>
-  <div className={s.login}>
-    <div className={s.menu}><a href="#">Вход</a></div>
-    <div className={s.menu}><a href="#">Регистрация</a></div>
-  </div>
+      <a href="#">
+        <img className={s.logo}
+             src="https://www.pngfind.com/pngs/m/84-841512_hacking-logo-png-logo-hacker-png-transparent-png.png"
+             alt="logo"/>
+      </a>
+      <h1>Native people</h1>
+      <div className={s.login}>
+        <div className={s.menu}><a href="#">Вход</a></div>
+        <div className={s.menu}><a href="#">Регистрация</a></div>
+      </div>
     </header>)
 }
 
