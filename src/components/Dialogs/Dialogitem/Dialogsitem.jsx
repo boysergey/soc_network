3dc23542be46ed411c7a8babcafb8979dd4c271e
boysergey <65461241+boysergey@users.noreply.github.com>
2021-06-15 21:52:06 +0300

    redux (file 'state'_1)

--- src/components/Dialogs/Dialogitem/Dialogsitem.jsx
+++ src/components/Dialogs/Dialogitem/Dialogsitem.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import s from './../Dialogs.module.css'
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 function DialogItem(props) {
-  return <div className={s.dialog + ' ' + s.active}><img className={s.paint}
-                                                         src='https://socialniesety.ru/files/images/components/articles_journal/originals/instagram/422/kak_sdelat_artavatarku_v_instagram_13.jpg'
-                                                         alt='avatar'/>
-    <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink></div>
+    return <div className={s.dialog + ' ' + s.active}>
+        <img className={s.paint}
+             src='https://socialniesety.ru/files/images/components/articles_journal/originals/instagram/422/kak_sdelat_artavatarku_v_instagram_13.jpg'
+             alt='avatar'/>
+        <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink></div>
 }
-
 
 
 export default DialogItem;
--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import s from './Friends.module.css'
+import DialogItem from "../Dialogs/Dialogitem/Dialogsitem";
 
 function Friends(props) {
   return(
-    <form>
-      <div className={s.form}>
-        <label htmlFor="formGroupExampleInput"/>
-        <input type="text" className="form-control" id="formGroupExampleInput"
-               placeholder="Введите имя вашего ДРУГА"/>
-      </div>
-      <div className="d-grid gap-2 col-6 mx-auto">
-        <button className="btn btn-primary" type="button">Отправить сообщение</button>
-      </div>
-    </form>
+   <div className={s.block}>
+       {props.dialogsData.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>)}
+   </div>
   )
 }
 export default Friends;
