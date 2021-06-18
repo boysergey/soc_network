23dcdfc78a8d1207d78f93dcde0429d1e6715a1f
boysergey <65461241+boysergey@users.noreply.github.com>
2021-06-18 13:49:03 +0300

    redux (file 'state'_2)

--- src/components/Content/My Post/Post/MyPost.jsx
+++ src/components/Content/My Post/Post/MyPost.jsx
@@ -4,31 +4,38 @@ import Post from './Post'
 
 const MyPost = (props) => {
 
-  let messageElements =
-    props.postsData.map(posts => <Post message={posts.message} likesCount={posts.likesCount} id = {posts.id}/>)
-  return (
+    let messageElements =
+        props.postsData.map(posts => <Post message={posts.message} likesCount={posts.likesCount} id={posts.id}/>)
+    let newPostElement = React.createRef()
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        props.addPost(text);
+        newPostElement.current.value = '';
+    }
+    return (
 
-    <div>
-      <div className={s.posts}>
-        <div className={s.post_title}>
-          Мои сообщения
+        <div>
+            <div className={s.posts}>
+                <div className={s.post_title}>
+                    Мои сообщения
+                </div>
+                <form>
+                    <div className={s.form}>
+                        <label htmlFor="formGroupExampleInput"/>
+                        <input type="text" className="form-control" id="formGroupExampleInput" ref={newPostElement}
+                               placeholder="Введите ваше сообщение"/>
+                    </div>
+                    <div className="d-grid gap-2 col-6 mx-auto">
+                        <button className="btn btn-primary btn-px" type="button" onClick={addPost}>Отправить сообщение
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div className="message">
+                {messageElements}
+            </div>
         </div>
-        <form>
-          <div className={s.form}>
-            <label htmlFor="formGroupExampleInput"/>
-            <input type="text" className="form-control" id="formGroupExampleInput"
-                   placeholder="Введите ваше сообщение"/>
-          </div>
-          <div className="d-grid gap-2 col-6 mx-auto">
-            <button className="btn btn-primary btn-px" type="button">Отправить сообщение</button>
-          </div>
-        </form>
-      </div>
-      <div className="message">
-        {messageElements}
-      </div>
-    </div>
-);
+    );
 }
 
 export default MyPost;
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,36 +1,48 @@
+import {rerenderEntireTree} from "../render";
 
-let state= {
-    postsData:[
-        { id: 1, message: 'Приветик', likesCount:85},
-        { id: 2, message: 'Как жизнь Дружище?', likesCount:12 },
-        { id: 3, message: 'Все отлично!!!', likesCount:1 },
-        { id: 4, message: 'Чем занимаешься?', likesCount:2 },
-        { id: 5, message: 'Делаю уроки.', likesCount:21 },
-        { id: 6, message: 'Помочь?', likesCount:6 },
-        { id: 7, message: 'Нет, спасибо.', likesCount:41 },
-        { id: 8, message: 'Если что, приши, помогу.', likesCount:5 },
-    ],
-    dialogsData : [
-        { id: 1, name: 'Andrey' },
-        { id: 2, name: 'Sasha' },
-        { id: 3, name: 'Viktor' },
-        { id: 4, name: 'Valera' },
-        { id: 5, name: 'Igor' },
-        { id: 6, name: 'Irina' },
-        { id: 7, name: 'Tatyana' },
-        { id: 8, name: 'Dmitriy' },
-    ],
-    messagesData : [
-        { id: 1, message: 'Привет' },
-        { id: 2, message: 'Как жизнь?' },
-        { id: 3, message: 'Все отлично!!!' },
-        { id: 4, message: 'Чем занимаешьяс?' },
-        { id: 5, message: 'Делаю уроки.' },
-        { id: 6, message: 'Помочь?' },
-        { id: 7, message: 'Нет, спасибо, я сам' },
-        { id: 8, message: 'Если что, приши, помогу.' },
-    ]
+
+let state = {
+        postsData: [
+            {id: 1, message: 'Приветик', likesCount: 85},
+            {id: 2, message: 'Как жизнь Дружище?', likesCount: 12},
+            {id: 3, message: 'Все отлично!!!', likesCount: 1},
+            {id: 4, message: 'Чем занимаешься?', likesCount: 2},
+            {id: 5, message: 'Делаю уроки.', likesCount: 21},
+            {id: 6, message: 'Помочь?', likesCount: 6},
+            {id: 7, message: 'Нет, спасибо.', likesCount: 41},
+            {id: 8, message: 'Если что, приши, помогу.', likesCount: 5},
+        ],
+        dialogsData: [
+            {id: 1, name: 'Andrey'},
+            {id: 2, name: 'Sasha'},
+            {id: 3, name: 'Viktor'},
+            {id: 4, name: 'Valera'},
+            {id: 5, name: 'Igor'},
+            {id: 6, name: 'Irina'},
+            {id: 7, name: 'Tatyana'},
+            {id: 8, name: 'Dmitriy'},
+        ],
+
+        messagesData: [
+            {id: 1, message: 'Привет'},
+            {id: 2, message: 'Как жизнь?'},
+            {id: 3, message: 'Все отлично!!!'},
+            {id: 4, message: 'Чем занимаешьяс?'},
+            {id: 5, message: 'Делаю уроки.'},
+            {id: 6, message: 'Помочь?'},
+            {id: 7, message: 'Нет, спасибо, я сам'},
+            {id: 8, message: 'Если что, пиши, помогу.'},
+        ]
 
 }
 
+export let addPost = (postMessage) => {
+    let newPost = {
+        id: 5,
+        message: postMessage,
+        likesCount: 0
+    };
+    state.postsData.push(newPost)
+    rerenderEntireTree(state);
+}
 export default state;
